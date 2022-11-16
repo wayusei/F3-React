4340f75a1f61f9b308625015fc5eee5cafb3f8e0
wayusei <r.roszam@gmail.com>
2022-11-15 22:01:14 -0600

    beduleando en react R1-S1

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import logo from './logo.svg';
+import image from './github.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={image} className="App-github" alt="logo-github" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          BEDU-leando el código en React
         </p>
+        <img src={logo} className="App-logo" alt="logo-react" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/wayusei"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Wayusei's Github profile
         </a>
       </header>
     </div>
